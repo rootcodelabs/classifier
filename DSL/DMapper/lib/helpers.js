e7848f4d437cc9255856aeb02ecbd676ae6cabaa
kalsara.magamage_roo <kalsara.magamage@rootcodelabs.com>
2024-06-12 21:05:25 +0530

    classifier-93 implement rest api inorder to communicate data through ruuter and data-mapper

---
+++ DSL/DMapper/lib/helpers.js
@@ -0,0 +1,13 @@
+import { createHmac,timingSafeEqual } from "crypto"
+
+export function verifySignature(payload, headers) {
+    const signature = headers['x-hub-signature'];
+    const SHARED_SECRET = 'wNc6HjKGu3RZXYNXqMTh';
+    const hmac = createHmac('sha256', Buffer.from(SHARED_SECRET, 'utf8'));
+    const payloadString = JSON.stringify(payload);
+    hmac.update(Buffer.from(payloadString, 'utf8'));
+    const computedSignature = hmac.digest('hex');
+    const computedSignaturePrefixed = "sha256=" + computedSignature;
+    const isValid = timingSafeEqual(Buffer.from(computedSignaturePrefixed, 'utf8'), Buffer.from(signature, 'utf8'));
+    return isValid;
+}
