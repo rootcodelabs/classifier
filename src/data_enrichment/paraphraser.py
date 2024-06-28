a7c1fa03e45388123d6888da078bf1463669930f
pamodaDilranga <pdrox9@gmail.com>
2024-06-28 08:11:07 +0530

    paraphraser config file split

--- src/data_enrichment/paraphraser.py
+++ src/data_enrichment/paraphraser.py
@@ -1,33 +1,37 @@
+import json
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 
 class Paraphraser:
-    def __init__(self):
-        self.model_name = "humarin/chatgpt_paraphraser_on_T5_base"
+    def __init__(self, config_path="config_files/paraphraser_config.json"):
+        with open(config_path, 'r') as file:
+            config = json.load(file)
+        
+        self.model_name = config["model_name"]
+        self.num_beams = config["num_beams"]
+        self.num_beam_groups = config["num_beam_groups"]
+        self.num_return_sequences = config["num_return_sequences"]
+        self.repetition_penalty = config["repetition_penalty"]
+        self.diversity_penalty = config["diversity_penalty"]
+        self.no_repeat_ngram_size = config["no_repeat_ngram_size"]
+        self.temperature = config["temperature"]
+        self.max_length = config["max_length"]
+        
         self.tokenizer = AutoTokenizer.from_pretrained(self.model_name)
         self.model = AutoModelForSeq2SeqLM.from_pretrained(self.model_name)
 
-    def generate_paraphrases(self, question,
-    num_beams=5,
-    num_beam_groups=5,
-    num_return_sequences=3,
-    repetition_penalty=10.0,
-    diversity_penalty=3.0,
-    no_repeat_ngram_size=2,
-    temperature=0.7,
-    max_length=128
-    ):
+    def generate_paraphrases(self, question):
         input_ids = self.tokenizer(
             f'paraphrase: {question}',
             return_tensors="pt", padding="longest",
-            max_length=max_length,
+            max_length=self.max_length,
             truncation=True,
         ).input_ids.to('cpu')
         
         outputs = self.model.generate(
-            input_ids, temperature=temperature, repetition_penalty=repetition_penalty,
-            num_return_sequences=num_return_sequences, no_repeat_ngram_size=no_repeat_ngram_size,
-            num_beams=num_beams, num_beam_groups=num_beam_groups,
-            max_length=max_length, diversity_penalty=diversity_penalty
+            input_ids, temperature=self.temperature, repetition_penalty=self.repetition_penalty,
+            num_return_sequences=self.num_return_sequences, no_repeat_ngram_size=self.no_repeat_ngram_size,
+            num_beams=self.num_beams, num_beam_groups=self.num_beam_groups,
+            max_length=self.max_length, diversity_penalty=self.diversity_penalty
         )
 
         res = self.tokenizer.batch_decode(outputs, skip_special_tokens=True)
