d5f7670137b0770ca03e9038e42be85cf37ee639
pamodaDilranga <pdrox9@gmail.com>
2024-07-01 13:20:31 +0530

    updating test cases to match automatic language detection

--- src/data_enrichment/test_data_enrichment.py
+++ src/data_enrichment/test_data_enrichment.py
@@ -9,26 +9,23 @@ class TestDataEnrichment(TestCase):
 
     def test_enrich_data_with_supported_language(self):
         original_sentence = "Hello, I hope this email finds you well. I am writing to inquire about the status of my application."
-        language = 'en'
 
-        enriched_data = self.enricher.enrich_data(original_sentence, language)
+        enriched_data = self.enricher.enrich_data(original_sentence)
         self.assertIsNotNone(enriched_data)
 
         for i, paraphrase in enumerate(enriched_data, 1):
             print(f"Paraphrase {i}: {paraphrase}")
 
     def test_enrich_data_with_unsupported_language(self):
-        original_sentence = "Hello, I hope this email finds you well. I am writing to inquire about the status of my application."
-        language = 'sl'
+        original_sentence = "Pozdravljeni, upam, da vas to e-pošto dobro najde. Pišem, da se pozanimam o stanju moje prijave."
 
-        enriched_data = self.enricher.enrich_data(original_sentence, language)
+        enriched_data = self.enricher.enrich_data(original_sentence)
         self.assertEqual(enriched_data, [])
 
     def test_enrich_data_with_estonian_sentence(self):
         original_sentence = "Tere, ma loodan, et see e-kiri leiab teid hästi. Ma kirjutan päringu staatuse minu taotluse. Ma esitasin kõik nõutavad dokumendid möödunud kuu, kuid ma pole saanud ühtegi värskendust sellest ajast. Kas saaksite palun mulle hetkeseisu kohta teavet anda?"
-        language = 'et'
 
-        enriched_data = self.enricher.enrich_data(original_sentence, language)
+        enriched_data = self.enricher.enrich_data(original_sentence)
         self.assertIsNotNone(enriched_data)
 
         for i, paraphrase in enumerate(enriched_data, 1):
@@ -36,9 +33,8 @@ class TestDataEnrichment(TestCase):
 
     def test_enrich_data_with_russian_sentence(self):
         original_sentence = "Привет, надеюсь, что это письмо находит вас в порядке. Я пишу с запросом о статусе моего заявления. Я подал все необходимые документы в прошлом месяце, но с тех пор не получил никаких обновлений. Не могли бы вы, пожалуйста, предоставить мне текущий статус?"
-        language = 'ru'
 
-        enriched_data = self.enricher.enrich_data(original_sentence, language)
+        enriched_data = self.enricher.enrich_data(original_sentence)
         self.assertIsNotNone(enriched_data)
 
         for i, paraphrase in enumerate(enriched_data, 1):
@@ -46,9 +42,8 @@ class TestDataEnrichment(TestCase):
 
     def test_enrich_data_with_polish_sentence(self):
         original_sentence = "Cześć, mam nadzieję, że to e-mail znajduje cię w dobrym zdrowiu. Piszę z zapytaniem o status mojej aplikacji. Złożyłem wszystkie wymagane dokumenty w zeszłym miesiącu, ale od tego czasu nie otrzymałem żadnej aktualizacji. Czy moglibyście podać mi bieżący status?"
-        language = 'pl'
 
-        enriched_data = self.enricher.enrich_data(original_sentence, language)
+        enriched_data = self.enricher.enrich_data(original_sentence)
         self.assertIsNotNone(enriched_data)
 
         for i, paraphrase in enumerate(enriched_data, 1):
@@ -56,9 +51,8 @@ class TestDataEnrichment(TestCase):
 
     def test_enrich_data_with_finnish_sentence(self):
         original_sentence = "Hei, toivottavasti tämä sähköposti tavoittaa teidät hyvin. Kirjoitan tiedustelun tilastani hakemukseni suhteen. Lähetin kaikki tarvittavat asiakirjat viime kuussa, mutta en ole sen jälkeen saanut päivityksiä. Voisitteko antaa minulle ajantasaisen tilanteen?"
-        language = 'fi'
 
-        enriched_data = self.enricher.enrich_data(original_sentence, language)
+        enriched_data = self.enricher.enrich_data(original_sentence)
         self.assertIsNotNone(enriched_data)
 
         for i, paraphrase in enumerate(enriched_data, 1):
