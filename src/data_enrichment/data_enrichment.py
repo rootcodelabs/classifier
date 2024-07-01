8c0700d6c20e176f7abd52c663e40794a8114cdd
pamodaDilranga <pdrox9@gmail.com>
2024-07-01 13:27:36 +0530

    updating data enrichment to have new configuration

--- src/data_enrichment/data_enrichment.py
+++ src/data_enrichment/data_enrichment.py
@@ -8,7 +8,7 @@ class DataEnrichment:
         self.translator = Translator()
         self.paraphraser = Paraphraser()
 
-    def enrich_data(self, text: str) -> List[str]:
+    def enrich_data(self, text: str, num_return_sequences: int = None) -> List[str]:
         supported_languages = ['en', 'et', 'ru', 'pl', 'fi']
         lang = detect(text)
 
@@ -17,10 +17,10 @@ class DataEnrichment:
             return []
 
         if lang == 'en':
-            paraphrases = self.paraphraser.generate_paraphrases(text)
+            paraphrases = self.paraphraser.generate_paraphrases(text, num_return_sequences)
         else:
             english_text = self.translator.translate(text, lang, 'en')
-            paraphrases = self.paraphraser.generate_paraphrases(english_text)
+            paraphrases = self.paraphraser.generate_paraphrases(english_text, num_return_sequences)
             translated_paraphrases = []
             for paraphrase in paraphrases:
                 translated_paraphrase = self.translator.translate(paraphrase, 'en', lang)
