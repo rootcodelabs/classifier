692818bf9b7c2a77f385de7d6781a456d4ad1420
erangi-ar <111747955+erangi-ar@users.noreply.github.com>
2024-07-03 00:14:33 +0530

    outlook consent sandalone app created

---
+++ outlook-consent/src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+       <button className='btn'>Login with Outlook</button>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
