29e61f081d0c7c3c95c6a237d4fa77c1a54c2694
kalsara.magamage_roo <kalsara.magamage@rootcodelabs.com>
2024-07-03 13:15:42 +0530

    classifier-97 add db password to environment variable

--- migrate.sh
+++ migrate.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --rm --network bykstack -v `pwd`/DSL/Liquibase/changelog:/liquibase/changelog -v `pwd`/DSL/Liquibase/master.yml:/liquibase/master.yml -v `pwd`/DSL/Liquibase/data:/liquibase/data liquibase/liquibase --defaultsFile=/liquibase/changelog/liquibase.properties --changelog-file=master.yml --url=jdbc:postgresql://users_db:5432/classifier?user=root --password=root update
+docker run --rm --network bykstack -v `pwd`/DSL/Liquibase/changelog:/liquibase/changelog -v `pwd`/DSL/Liquibase/master.yml:/liquibase/master.yml -v `pwd`/DSL/Liquibase/data:/liquibase/data liquibase/liquibase --defaultsFile=/liquibase/changelog/liquibase.properties --changelog-file=master.yml --url=jdbc:postgresql://users_db:5432/classifier?user=root --password=$DB_PASSWORD update
