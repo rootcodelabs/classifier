639d937e82d94e78bae90d4eec4dcbaa616ed648
kalsara.magamage_roo <kalsara.magamage@rootcodelabs.com>
2024-07-11 15:23:48 +0530

    ESCLASS-139: integration issue in outlook fixed by setting config and implement refresh token save flow

--- migrate.sh
+++ migrate.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# Define the path where the SQL file will be generated
+SQL_FILE="DSL/Liquibase/data/update_refresh_token.sql"
+
+# Read the OUTLOOK_REFRESH_KEY value from the INI file
+OUTLOOK_REFRESH_KEY=$(awk -F '=' '/OUTLOOK_REFRESH_KEY/ {print $2}' constants.ini | xargs)
+
+# Generate a SQL script with the extracted value
+cat << EOF > "$SQL_FILE"
+-- Update the refresh token in the database
+UPDATE integration_status
+SET token = '$OUTLOOK_REFRESH_KEY'
+WHERE platform='OUTLOOK';
+EOF
+
 # Function to parse ini file and extract the value for a given key under a given section
 get_ini_value() {
     local file=$1
@@ -12,4 +26,4 @@ INI_FILE="constants.ini"
 DB_PASSWORD=$(get_ini_value "$INI_FILE" "DB_PASSWORD")
 
 
-docker run --rm --network bykstack -v `pwd`/DSL/Liquibase/changelog:/liquibase/changelog -v `pwd`/DSL/Liquibase/master.yml:/liquibase/master.yml -v `pwd`/DSL/Liquibase/data:/liquibase/data liquibase/liquibase --defaultsFile=/liquibase/changelog/liquibase.properties --changelog-file=master.yml --url=jdbc:postgresql://users_db:5432/classifier?user=postgres --password=$DB_PASSWORD update
+docker run --rm --network bykstack -v `pwd`/DSL/Liquibase/changelog:/liquibase/changelog -v `pwd`/DSL/Liquibase/master.yml:/liquibase/master.yml -v `pwd`/DSL/Liquibase/data:/liquibase/data liquibase/liquibase --defaultsFile=/liquibase/changelog/liquibase.properties --changelog-file=master.yml --url=jdbc:postgresql://users_db:5432/classifier?user=root --password=$DB_PASSWORD update
