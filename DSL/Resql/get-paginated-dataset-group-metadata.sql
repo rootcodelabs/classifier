03fed197645d455d7afcf091be5facdcd1cfaa91
kalsara.magamage_roo <kalsara.magamage@rootcodelabs.com>
2024-07-18 15:37:58 +0530

    ESCLASS-137: add filters to get dataset overview API

--- DSL/Resql/get-paginated-dataset-group-metadata.sql
+++ DSL/Resql/get-paginated-dataset-group-metadata.sql
@@ -11,5 +11,13 @@ SELECT  dt.id as dg_id,
        dt.validation_status,
        CEIL(COUNT(*) OVER() / :page_size::DECIMAL) AS total_pages
 FROM "dataset_group_metadata" dt
-ORDER BY CASE WHEN :sorting = 'name asc' THEN dt.group_name END ASC
+WHERE
+    (:major_version = -1 OR dt.major_version = :major_version)
+    AND (:minor_version = -1 OR dt.minor_version = :minor_version)
+    AND (:patch_version = -1 OR dt.patch_version = :patch_version)
+    AND (:validation_status = 'all' OR dt.validation_status = :validation_status::Validation_Status)
+    AND (:group_name = 'all' OR dt.group_name = :group_name)
+ORDER BY
+    CASE WHEN :sorting = 'asc' THEN dt.group_name END ASC,
+    CASE WHEN :sorting = 'dsc' THEN dt.group_name END DESC
 OFFSET ((GREATEST(:page, 1) - 1) * :page_size) LIMIT :page_size;
