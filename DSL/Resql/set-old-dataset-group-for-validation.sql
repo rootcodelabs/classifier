85b85752f4a96c9ef5b2abc59fb8b059dc92653b
kalsara.magamage_roo <kalsara.magamage@rootcodelabs.com>
2024-07-20 19:05:14 +0530

    ESCLASS-125: minor version revamp implementation

--- DSL/Resql/set-old-dataset-group-for-validation.sql
+++ DSL/Resql/set-old-dataset-group-for-validation.sql
@@ -1,8 +1,24 @@
-UPDATE dataset_group_metadata
-SET
-    minor_version = minor_version + 1,
-    enable_allowed = false,
-    validation_status = 'in-progress'::Validation_Status,
-    is_enabled = false,
-    raw_data_location = :s3_file_path
-WHERE id = :id;
+WITH update_latest AS (
+    UPDATE dataset_group_metadata
+    SET latest = false
+    WHERE group_key = :group_key
+    RETURNING 1
+),
+update_specific AS (
+    UPDATE dataset_group_metadata
+    SET
+        minor_version = (
+            SELECT COALESCE(MAX(minor_version), 0) + 1
+            FROM dataset_group_metadata
+            WHERE group_key = :group_key
+        ),
+        enable_allowed = false,
+        validation_status = 'in-progress'::Validation_Status,
+        is_enabled = false,
+        patch_version = 0,
+        latest = true,
+        last_updated_timestamp = to_timestamp(:last_updated_timestamp)::timestamp with time zone
+    WHERE id = :id
+    RETURNING 1
+)
+SELECT 1;
--- DSL/Resql/snapshot-dataset-group.sql
+++ DSL/Resql/snapshot-dataset-group.sql
@@ -1,15 +1,15 @@
 INSERT INTO dataset_group_metadata (
-    group_name, major_version, minor_version, patch_version, latest,
+    group_name, group_key, major_version, minor_version, patch_version, latest,
     is_enabled, enable_allowed, last_model_trained, created_timestamp,
-    last_updated_timestamp, last_used_for_training, validation_status,
+    last_updated_timestamp, last_trained_timestamp, validation_status,
     validation_errors, processed_data_available, raw_data_available,
     num_samples, num_pages, raw_data_location, preprocess_data_location,
     validation_criteria, class_hierarchy, connected_models
 )
 SELECT
-    group_name, major_version, minor_version, patch_version, latest,
+    group_name, group_key, major_version, minor_version, patch_version, false AS latest,
     is_enabled, enable_allowed, last_model_trained, created_timestamp,
-    last_updated_timestamp, last_used_for_training, validation_status,
+    last_updated_timestamp, last_trained_timestamp, validation_status,
     validation_errors, processed_data_available, raw_data_available,
     num_samples, num_pages, raw_data_location, preprocess_data_location,
     validation_criteria, class_hierarchy, connected_models
