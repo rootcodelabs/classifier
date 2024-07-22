8ea2bc3727df02e7f8b0a47a4b073a061dfdb708
kalsara.magamage_roo <kalsara.magamage@rootcodelabs.com>
2024-07-22 18:43:30 +0530

    ESCLASS-118-implement gat dataset group meta data by id

--- DSL/Resql/get-dataset-group-metadata-by-id.sql
+++ DSL/Resql/get-dataset-group-metadata-by-id.sql
@@ -1,2 +1,3 @@
-SELECT enable_allowed
-FROM dataset_group_metadata  WHERE id =:id;
+SELECT id, group_name, major_version, minor_version, patch_version, latest, is_enabled, num_samples, enable_allowed,
+ validation_status, validation_errors, connected_models, validation_criteria, class_hierarchy
+FROM dataset_group_metadata WHERE id = :id;
