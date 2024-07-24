780398e8ac401d71fa1b677ad176b3c1a2a84015
pamodaDilranga <pdrox9@gmail.com>
2024-07-24 12:55:50 +0530

    cron

--- DSL/CronManager/script/data_processor_exec.sh
+++ DSL/CronManager/script/data_processor_exec.sh
@@ -6,17 +6,6 @@ cd "$(dirname "$0")"
 # Source the constants from the ini file
 source ../config/config.ini
 
-# Create JSON payload
-json_payload=$(jq -n \
-                  --arg dgID "$dgId" \
-                  --arg authCookie "$customJwtCookie" \
-                  '{dgID: $dgID|tonumber, authCookie: $authCookie}')
-
-# Send POST request
-response=$(curl -s -X POST "$INIT_DATESET_PROCESSOR_API" \
-                 -H "Content-Type: application/json" \
-                 -b "customJwtCookie=$customJwtCookie" \
-                 -d "$json_payload")
-
-# Print response
-echo "Response from API: $response"
+echo "dgID $dgId"
+echo "cookie $cookie"
+echo "API $INIT_DATESET_PROCESSOR_API"
