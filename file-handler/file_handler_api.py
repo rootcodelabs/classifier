57f5030fb2dfb5ebf21031bd810855b57170334b
pamodaDilranga <pdrox9@gmail.com>
2024-08-05 00:20:12 +0530

    completed stopwords

--- file-handler/file_handler_api.py
+++ file-handler/file_handler_api.py
@@ -12,6 +12,11 @@ from constants import (
     S3_UPLOAD_FAILED, S3_DOWNLOAD_FAILED, JSON_EXT, YAML_EXT, YML_EXT, XLSX_EXT
 )
 from s3_ferry import S3Ferry
+import yaml
+import pandas as pd
+from typing import List
+from io import BytesIO, TextIOWrapper
+
 
 app = FastAPI()
 
@@ -310,15 +315,38 @@ async def upload_and_copy(request: Request, copyPayload: CopyPayload):
         upload_success["saved_file_path"] = f"/dataset/{new_dg_id}/"
         return JSONResponse(status_code=200, content=upload_success)
 
+def extract_stop_words(file: UploadFile) -> List[str]:
+    file_converter = FileConverter()
+    file_type = file_converter._detect_file_type(file.filename)
+    
+    if file_type == 'txt':
+        content = file.file.read().decode('utf-8')
+        return [word.strip() for word in content.split(',')]
+    elif file_type == 'json':
+        content = json.load(file.file)
+        return content if isinstance(content, list) else []
+    elif file_type == 'yaml':
+        content = yaml.safe_load(file.file)
+        return content if isinstance(content, list) else []
+    elif file_type == 'xlsx':
+        content = file.file.read()
+        excel_file = BytesIO(content)
+        data = pd.read_excel(excel_file, sheet_name=None)
+        stop_words = []
+        for sheet in data:
+            stop_words.extend(data[sheet].stack().tolist())
+        return stop_words
+    else:
+        raise HTTPException(status_code=400, detail="Unsupported file type")
+
+
 @app.post("/datasetgroup/data/import/stop-words")
 async def import_stop_words(request: Request, stopWordsFile: UploadFile = File(...)):
     try:
         cookie = request.cookies.get("customJwtCookie")
         await authenticate_user(f'customJwtCookie={cookie}')
 
-        file_content = await stopWordsFile.read()
-        
-        words_list = [word.strip() for word in file_content.decode('utf-8').split(',')]
+        words_list = extract_stop_words(stopWordsFile)
 
         url = IMPORT_STOPWORDS_URL
         headers = {
@@ -330,10 +358,10 @@ async def import_stop_words(request: Request, stopWordsFile: UploadFile = File(.
 
         if response.status_code == 200:
             response_data = response.json()
-            if response_data['operationSuccessful']:
+            if response_data['response']['operationSuccessful']:
                 return response_data
-            elif response_data['duplicate']:
-                duplicate_items = response_data['duplicateItems']
+            elif response_data['response']['duplicate']:
+                duplicate_items = response_data['response']['duplicateItems']
                 new_words_list = [word for word in words_list if word not in duplicate_items]
                 if new_words_list:
                     response = requests.post(url, headers=headers, json={"stopWords": new_words_list})
@@ -352,8 +380,7 @@ async def delete_stop_words(request: Request, stopWordsFile: UploadFile = File(.
         cookie = request.cookies.get("customJwtCookie")
         await authenticate_user(f'customJwtCookie={cookie}')
 
-        file_content = await stopWordsFile.read()
-        words_list = [word.strip() for word in file_content.decode('utf-8').split(',')]
+        words_list = extract_stop_words(stopWordsFile)
 
         url = DELETE_STOPWORDS_URL
         headers = {
@@ -365,10 +392,10 @@ async def delete_stop_words(request: Request, stopWordsFile: UploadFile = File(.
 
         if response.status_code == 200:
             response_data = response.json()
-            if response_data['operationSuccessful']:
+            if response_data['response']['operationSuccessful']:
                 return response_data
-            elif response_data['nonexistent']:
-                nonexistent_items = response_data['nonexistentItems']
+            elif response_data['response']['nonexistent']:
+                nonexistent_items = response_data['response']['nonexistentItems']
                 new_words_list = [word for word in words_list if word not in nonexistent_items]
                 if new_words_list:
                     response = requests.post(url, headers=headers, json={"stopWords": new_words_list})
