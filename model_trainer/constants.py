e58eb8ad9f809d2eeb05d2f384dee9e0b5c09ef3
Thirunayan22 <thirunayan22@gmail.com>
2024-08-19 17:02:37 +0530

    fixed training pipeline to save and store models after training. Integrated S3 ferry with Cron Manager. refactored config and constants .ini files.

--- model_trainer/constants.py
+++ model_trainer/constants.py
@@ -1,8 +1,29 @@
 
-URL_DATA = "http://file-handler:8000/datasetgroup/data/download/json"
+#TODO - REFACTOR CODE TO CREATE A GENERIC FUNCTION HERE WHICH WILL CONSTRUCT AND RETURN THE CONSTANTS IN A DICTIONARY WHICH CAN BE REFERENCED IN ALL PARTS OF THE CODE
 
-URL_HIERARCHY = "http://ruuter-private:8088/classifier/datasetgroup/group/metadata"
+DATA_DOWNLOAD_ENDPOINT = "http://file-handler:8000/datasetgroup/data/download/json"
 
-URL_MODEL= "http://ruuter-private:8088/classifier/datamodel/metadata"
+GET_DATASET_METADATA_ENDPOINT = "http://ruuter-private:8088/classifier/datasetgroup/group/metadata"
 
-URL_DEPLOY = "http://localhost:8088/classifier/datamodel/deployment/{deployment_platform}/update"
+GET_MODEL_METADATA_ENDPOINT= "http://ruuter-private:8088/classifier/datamodel/metadata"
+
+DEPLOYMENT_ENDPOINT = "http://ruuter-private:8088/classifier/datamodel/deployment/{deployment_platform}/update"
+
+TRAINING_LOGS_PATH = "/app/model_trainer/training_logs.log"
+
+MODEL_RESULTS_PATH = "/shared/model_trainer/results" #stored in the shared folder which is connected to s3-ferry
+
+LOCAL_BASEMODEL_TRAINED_LAYERS_SAVE_PATH = "/shared/model_trainer/results/{model_id}/trained_base_model_layers" #stored in the shared folder which is connected to s3-ferry
+
+LOCAL_CLASSIFICATION_LAYER_SAVE_PATH = "/shared/model_trainer/results/{model_id}/classifier_layers" #stored in the shared folder which is connected to s3-ferry
+
+LOCAL_LABEL_ENCODER_SAVE_PATH = "/shared/model_trainer/results/{model_id}/label_encoders" #stored in the shared folder which is connected to s3-ferry
+
+S3_FERRY_MODEL_STORAGE_PATH = "/models" #folder path in s3 bucket
+
+
+BASE_MODEL_FILENAME = "base_model_trainable_layers_{model_id}"
+
+CLASSIFIER_MODEL_FILENAME = "classifier_{model_id}.pth"
+
+
--- model_trainer/model_trainer.py
+++ model_trainer/model_trainer.py
@@ -6,106 +6,166 @@ import torch
 import pickle
 import shutil
 from s3_ferry import S3Ferry
-from constants import URL_MODEL, URL_DEPLOY
+from constants import  GET_MODEL_METADATA_ENDPOINT, DEPLOYMENT_ENDPOINT,TRAINING_LOGS_PATH, MODEL_RESULTS_PATH, \
+                        LOCAL_BASEMODEL_TRAINED_LAYERS_SAVE_PATH,LOCAL_CLASSIFICATION_LAYER_SAVE_PATH, \
+                        LOCAL_LABEL_ENCODER_SAVE_PATH, S3_FERRY_MODEL_STORAGE_PATH
+from loguru import logger
+
+logger.add(sink=TRAINING_LOGS_PATH)
+
+#TODO - REFACTOR CODE TO CREATE A GENERIC FUNCTION HERE WHICH WILL CONSTRUCT AND RETURN THE CONSTANTS IN A DICTIONARY WHICH CAN BE REFERENCED IN ALL PARTS OF THE CODE
 
 class ModelTrainer:
-    def __init__(self) -> None:
-        
-        cookie = os.environ.get('COOKIE')
-        newModelId = os.environ.get('NEW_MODEL_ID')
-        oldModelId = os.environ.get('OLD_MODEL_ID')
+    def __init__(self, cookie, new_model_id,old_model_id) -> None:
 
-        model_url = URL_MODEL
+        model_url = GET_MODEL_METADATA_ENDPOINT
 
-        self.newModelId = newModelId
-        self.oldModelId = oldModelId
-        cookies = {'customJwtCookie': cookie}
+        self.new_model_id = new_model_id
+        self.old_model_id = old_model_id
         self.cookie = cookie
-        response = requests.get(model_url, params = {'modelId': newModelId}, cookies=cookies)
+        
+        cookies = {'customJwtCookie': cookie}
+
+        logger.info("GETTING MODEL METADATA")
+
+        response = requests.get(model_url, params = {'modelId': self.new_model_id}, cookies=cookies)
+                    
         if response.status_code == 200:
             self.model_details = response.json()
-            print("success")
+            logger.info("SUCCESSFULLY RECIEVED MODEL DETAILS")
         else:
-            print(f"Failed with status code: {response.status_code}")
+            
+            logger.error(f"FAILED WITH STATUS CODE: {response.status_code}")
+            logger.error(f"RESPONSE: {response.text}")
+
+            raise RuntimeError(f"RESPONSE : {response.text}")
+        
+    @staticmethod
+    def create_training_folders(folder_paths):
+
+        logger.info("CREATING FOLDER PATHS")
+
+        try:
+
+            for folder_path in folder_paths:
+                if not os.path.exists(folder_path):
+                    os.makedirs(folder_path)
+            
+            logger.success(f"SUCCESSFULLY CREATED MODEL FOLDER PATHS : {folder_paths}")
+
+        except Exception as e:
+
+            logger.error(f"FAILED TO CREATE MODEL FOLDER PATHS : {folder_paths}")
+            raise RuntimeError(e)
 
     def train(self):
+
         s3_ferry = S3Ferry()
-        dgId = self.model_details['response']['data'][0]['connectedDgId']
-        data_pipeline = DataPipeline(dgId, self.cookie)
+        dg_id = self.model_details['response']['data'][0]['connectedDgId']
+        data_pipeline = DataPipeline(dg_id, self.cookie)
         dfs = data_pipeline.create_dataframes()
-        models_dets,_  = data_pipeline.models_and_filters()
+        models_inference_metadata,_  = data_pipeline.models_and_filters()
         models_to_train = self.model_details['response']['data'][0]['baseModels']
 
-        results_rt_paths = [f"results/saved_models", f"results/classifiers", f"results/saved_label_encoders"]
-        for path in results_rt_paths:
-            if not os.path.exists(path):
-                os.makedirs(path)
-        
-        with open('results/models_dets.pkl', 'wb') as file:
-            pickle.dump(models_dets, file)
+        local_basemodel_layers_save_path = LOCAL_BASEMODEL_TRAINED_LAYERS_SAVE_PATH.format(model_id=self.new_model_id)
+        local_classification_layer_save_path = LOCAL_CLASSIFICATION_LAYER_SAVE_PATH.format(model_id=self.new_model_id)
+        local_label_encoder_save_path = LOCAL_LABEL_ENCODER_SAVE_PATH.format(model_id=self.new_model_id)
 
-        models_list = []
-        classifiers_list = []
-        label_encoders_list = []
+
+        ModelTrainer.create_training_folders([local_basemodel_layers_save_path,
+                                              local_classification_layer_save_path,
+                                              local_label_encoder_save_path])
+        
+        with open(f'{MODEL_RESULTS_PATH}/{self.new_model_id}/models_dets.pkl', 'wb') as file:
+            pickle.dump(models_inference_metadata, file)
+
+        selected_models = []
+        selected_classifiers = []
+        selected_label_encoders = []
         average_accuracy = []
+        logger.info(f"MODELS TO BE TRAINED: {models_to_train}")
+
         for i in range(len(models_to_train)):
             training_pipeline =  TrainingPipeline(dfs, models_to_train[i])
             metrics, models, classifiers, label_encoders = training_pipeline.train()
-            models_list.append(models)
-            classifiers_list.append(classifiers)
-            label_encoders_list.append(label_encoders)
+            selected_models.append(models)
+            selected_classifiers.append(classifiers)
+            selected_label_encoders.append(label_encoders)
             average = sum(metrics[1]) / len(metrics[1])
             average_accuracy.append(average)
-            
+
         max_value_index = average_accuracy.index(max(average_accuracy))
-        best_models = models_list[max_value_index]
-        best_classifiers = classifiers_list[max_value_index]
-        best_label_encoders = label_encoders_list[max_value_index]
-        model_name = models_to_train[max_value_index]
-        for i, (model, classifier, label_encoder) in enumerate(zip(best_models, best_classifiers, best_label_encoders)):
-            if model_name == 'distil-bert':
-                torch.save(model, f"results/saved_models/last_two_layers_dfs_{i}.pth")
-            elif model_name == 'roberta':
-                torch.save(model, f"results/saved_models/last_two_layers_dfs_{i}.pth")
-            elif model_name == 'bert':
-                torch.save(model, f"results/saved_models/last_two_layers_dfs_{i}.pth")
+        best_model_base = selected_models[max_value_index]
+        best_model_classifier = selected_classifiers[max_value_index]
+        best_model_label_encoder = selected_label_encoders[max_value_index]
+        best_model_name = models_to_train[max_value_index]
 
-            torch.save(classifier, f"results/classifiers/classifier_{i}.pth")
+        logger.info("TRAINING COMPLETE")
+        logger.info(f"THE BEST PERFORMING MODEL IS {best_model_name}")
 
-            label_encoder_path = f"results/saved_label_encoders/label_encoder_{i}.pkl"
-            with open(label_encoder_path, 'wb') as file:
-                pickle.dump(label_encoder, file)
+        torch.save(best_model_base, f"{local_basemodel_layers_save_path}/base_model_trainable_layers_{self.new_model_id}.pth")
+        torch.save(best_model_classifier, f"{local_classification_layer_save_path}/classifier_{self.new_model_id}.pth")
         
-        shutil.make_archive(f"{str(self.newModelId)}", 'zip', f"results")
-        save_location = f"shared/models/{str(self.newModelId)}/{str(self.newModelId)}.zip"
-        source_location = f"{str(self.newModelId)}.zip"
+        label_encoder_path = f"{local_label_encoder_save_path}/label_encoder_{self.new_model_id}.pkl"
+        with open(label_encoder_path, 'wb') as file:
+            pickle.dump(best_model_label_encoder, file)
+
+        
+        model_zip_path = f"{MODEL_RESULTS_PATH}/{str(self.new_model_id)}"
+
+        shutil.make_archive(base_name=model_zip_path, root_dir=model_zip_path, format="zip") 
+        
+        save_location = f"{S3_FERRY_MODEL_STORAGE_PATH}/{str(self.new_model_id)}/{str(self.new_model_id)}.zip"
+        source_location = f"{MODEL_RESULTS_PATH.replace('/shared/','')}/{str(self.new_model_id)}.zip" # Removing 'shared/' path here so that S3 ferry source file path works without any issue
+
+        logger.info("INITIATING MODEL UPLOAD TO S3")
+        logger.info(f"SOURCE LOCATION - {source_location}")
+        logger.info(f"S3 SAVE LOCATION - {save_location}")
+        
         response = s3_ferry.transfer_file(save_location, "S3", source_location, "FS")
+        
         if response.status_code == 201:
-            upload_status = {"message": "Model File Uploaded Successfully!", "saved_file_path": save_location}
+            logger.info(f"MODEL FILE UPLOADED SUCCESSFULLY TO {save_location}")
         
         else:
-            upload_status = {"message": "failed to Upload Model File!"}
+            logger.error(f"MODEL FILE UPLOAD TO {save_location} FAILED")
+            logger.error(f"RESPONSE: {response.text}")
+            raise RuntimeError(f"RESPONSE STATUS: {response.text}")
         
-        DeploymentPlatform = self.model_details['response']['data'][0]['deploymentEnv']
+ 
+        deployment_platform = self.model_details['response']['data'][0]['deploymentEnv']
 
-        deploy_url = URL_DEPLOY.format(deployment_platform = DeploymentPlatform)
+        logger.info(f"INITIATING DEPLOYMENT TO {deployment_platform}")
+
+        deploy_url = DEPLOYMENT_ENDPOINT.format(deployment_platform = deployment_platform)
 
     
-        if self.oldModelId is not None:
+    ## CODE SHOULD BE UPDATED TO CHECK WHETHER old_model_id  == new_model_id (because that is how ruuter sends the request if it's a model create operation) 
+        if self.old_model_id is not None:
             
             payload = {
-                "modelId": self.newModelId,
+                "modelId": self.new_model_id,
                 "replaceDeployment": True,
-                "replaceDeploymentPlatform":DeploymentPlatform,
-                "bestModelName":model_name
+                "replaceDeploymentPlatform":deployment_platform,
+                "bestModelName":best_model_name
             }
         
         else:
             payload = {
-                "modelId": self.newModelId,
+                "modelId": self.new_model_id,
                 "replaceDeployment": False,
-                "replaceDeploymentPlatform": DeploymentPlatform,
-                "bestModelName":model_name
+                "replaceDeploymentPlatform": deployment_platform,
+                "bestModelName":best_model_name
             }
 
         response = requests.post(deploy_url, json=payload)
+
+        if response.status_code == 201 or response.status_code == 200:
+            logger.info(f"{deployment_platform} DEPLOYMENT SUCCESSFUL")
+        
+        else:
+            logger.error(f"{deployment_platform} DEPLOYMENT FAILED")
+            logger.info(f"RESPONSE: {response.text}")
+
+            raise RuntimeError(f"RESPONSE : {response.text}")
+
