753fe28520b7c7d027ce9b1e3b9aa1324dacc693
erangi-ar <111747955+erangi-ar@users.noreply.github.com>
2024-08-23 20:06:37 +0530

    bug fixes

--- GUI/src/utils/endpoints.ts
+++ GUI/src/utils/endpoints.ts
@@ -29,6 +29,7 @@ export const datasetsEndpoints = {
     `/classifier/datasetgroup/update/minor`,
   DATASET_GROUP_MAJOR_UPDATE: (): string =>
     `/classifier/datasetgroup/update/major`,
+  DELETE_DATASET_GROUP:(): string =>`classifier/datasetgroup/delete`,
   GET_STOP_WORDS: (): string => `/classifier/datasetgroup/stop-words`,
   POST_STOP_WORDS: (): string => `/classifier/datasetgroup/update/stop-words`,
   DELETE_STOP_WORD: (): string => `/classifier/datasetgroup/delete/stop-words`,
