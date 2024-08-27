14501bd1ab3cae6f7e177a9393f0bdb371eb2161
erangi-ar <111747955+erangi-ar@users.noreply.github.com>
2024-08-27 12:37:05 +0530

    sonar cloud fixes

--- GUI/src/assets/UserIcon.tsx
+++ GUI/src/assets/UserIcon.tsx
@@ -12,16 +12,16 @@ const UserIcon = () => {
       <path
         d="M11.6668 12.75V11.5833C11.6668 10.9645 11.421 10.371 10.9834 9.93342C10.5458 9.49583 9.95234 9.25 9.3335 9.25H4.66683C4.04799 9.25 3.4545 9.49583 3.01691 9.93342C2.57933 10.371 2.3335 10.9645 2.3335 11.5833V12.75"
         stroke="white"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M6.99984 6.91667C8.2885 6.91667 9.33317 5.872 9.33317 4.58333C9.33317 3.29467 8.2885 2.25 6.99984 2.25C5.71117 2.25 4.6665 3.29467 4.6665 4.58333C4.6665 5.872 5.71117 6.91667 6.99984 6.91667Z"
         stroke="white"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );
