5a3804070ae1e0944101b970951ed532da1d7d54
kalsara.magamage_roo <kalsara.magamage@rootcodelabs.com>
2024-08-30 12:32:47 +0530

    remove-unwanted-files: clean the code

--- DSL/DMapper/lib/helpers.js
+++ DSL/DMapper/lib/helpers.js
@@ -1,20 +1,5 @@
 import { createHmac, timingSafeEqual, randomBytes } from "crypto";
 
-export function verifySignature(payload, headers, secret) {
-  const signature = headers["x-hub-signature"];
-  const SHARED_SECRET = secret;
-  const hmac = createHmac("sha256", Buffer.from(SHARED_SECRET, "utf8"));
-  const payloadString = JSON.stringify(payload);
-  hmac.update(Buffer.from(payloadString, "utf8"));
-  const computedSignature = hmac.digest("hex");
-  const computedSignaturePrefixed = "sha256=" + computedSignature;
-  const isValid = timingSafeEqual(
-    Buffer.from(computedSignaturePrefixed, "utf8"),
-    Buffer.from(signature, "utf8")
-  );
-  return isValid;
-}
-
 export function getAuthHeader(username, token) {
   const auth = `${username}:${token}`;
   const encodedAuth = Buffer.from(auth).toString("base64");
