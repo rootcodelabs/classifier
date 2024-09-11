4204f725e529138e5d28a0e65b2fc6ce2a58b4d9
pamodaDilranga <pdrox9@gmail.com>
2024-09-11 22:40:29 +0530

    main file with print logging

--- model_trainer/main.py
+++ model_trainer/main.py
@@ -2,13 +2,18 @@ import os
 from loguru import logger
 
 try:
+    print("INIT STARTED MAIN.PY")
     logger.add(sink=TRAINING_LOGS_PATH)
     logger.info("INIT STARTED MAIN.PY")
+    print("INIT STARTED MAIN.PY - Log recorded")
+
     from model_trainer import ModelTrainer
     from constants import TRAINING_LOGS_PATH
     import json
 
     logger.info("PROCESS STARTED MAIN.PY")
+    print("PROCESS STARTED MAIN.PY - Log recorded")
+    
     cookie = str(os.environ.get('cookie'))
     new_model_id = int(os.environ.get('newModelId'))
     old_model_id = int(os.environ.get('modelId'))
@@ -19,24 +24,42 @@ try:
     model_details = json.loads(os.environ.get('modelDetails'))
 
     logger.info(f"COOKIE - {cookie}")
+    print(f"COOKIE - {cookie} - Log recorded")
+
     logger.info(f"OLD MODEL ID {old_model_id}")
+    print(f"OLD MODEL ID {old_model_id} - Log recorded")
+
     logger.info(f"NEW MODEL ID - {new_model_id}")
+    print(f"NEW MODEL ID - {new_model_id} - Log recorded")
+
     logger.info(f"UPDATE TYPE  - {update_type}")
-    logger.info(f"PREVIOUSE DEPLOYMENT ENV - {prev_deployment_env}")
+    print(f"UPDATE TYPE  - {update_type} - Log recorded")
+
+    logger.info(f"PREVIOUS DEPLOYMENT ENV - {prev_deployment_env}")
+    print(f"PREVIOUS DEPLOYMENT ENV - {prev_deployment_env} - Log recorded")
+
     logger.info(f"PROGRESS SESSION ID - {progress_session_id}")
+    print(f"PROGRESS SESSION ID - {progress_session_id} - Log recorded")
+
     logger.info(f"MODEL DETAILS - {model_details}")
+    print(f"MODEL DETAILS - {model_details} - Log recorded")
+
     logger.info(type(model_details))
+    print(f"MODEL DETAILS TYPE - {type(model_details)} - Log recorded")
 
     logger.info(f"ENTERING MODEL TRAINER SCRIPT FOR MODEL ID  - {old_model_id}")
+    print(f"ENTERING MODEL TRAINER SCRIPT FOR MODEL ID  - {old_model_id} - Log recorded")
 
-
-    trainer = ModelTrainer(cookie=cookie,new_model_id=new_model_id,
+    trainer = ModelTrainer(cookie=cookie, new_model_id=new_model_id,
                         old_model_id=old_model_id, prev_deployment_env=prev_deployment_env,
                         update_type=update_type, progress_session_id=progress_session_id, 
                         current_deployment_platform=deployment_env, model_details=model_details)
     trainer.train()
 
     logger.info("TRAINING SCRIPT COMPLETED")
-except Exception as e:
-    logger.info(f"Error in MAIN.PY : {e}")
+    print("TRAINING SCRIPT COMPLETED - Log recorded")
 
+except Exception as e:
+    print(f"Error in MAIN.PY : {e}")
+    logger.info(f"Error in MAIN.PY : {e}")
+    print(f"Error in MAIN.PY - Log recorded")
